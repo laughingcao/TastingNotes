68249803e182f481a3e244422e3dd45f0f7f706e
William Cao <williamacao@gmail.com>
2021-08-29 11:00:08 -0700

    added get route to spirits#notes

--- tasting_notes_api/config/routes.rb
+++ tasting_notes_api/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  get '/notes/:id', to: 'spirits#notes'
+
   resources :tasting_notes
   resources :spirits
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
