1ad62589a063c147f2e1c3066d3c207c4b4a203b
William Cao <williamacao@gmail.com>
2021-08-28 19:39:35 -0700

    testing new format

--- tasting_notes_client/index.js
+++ tasting_notes_client/index.js
@@ -1,24 +1,60 @@
-const base_url = "http://127.0.0.1:3000"
-const tastingNoteService = new TastingNoteService(base_url)
-const spiritService = new SpiritService(base_url)
+// const base_url = "http://127.0.0.1:3000"
+// const tastingNoteService = new TastingNoteService(base_url)
+// const spiritService = new SpiritService(base_url)
 
-Spirit.spiritForm.addEventListener('submit', handleSpiritSubmit)
-TastingNote.tastingNoteForm.addEventListener('submit', handleTastingNoteSubmit)
+// Spirit.spiritForm.addEventListener('submit', handleSpiritSubmit)
+// TastingNote.tastingNoteForm.addEventListener('submit', handleTastingNoteSubmit)
 
-spiritService.getSpirits()
-Spirit.renderSpiritForm()
+// spiritService.getSpirits()
+// Spirit.renderSpiritForm()
 
-tastingNoteService.getTastingNotes()
-TastingNote.renderTastingNoteForm()
+// tastingNoteService.getTastingNotes()
+// TastingNote.renderTastingNoteForm()
 
-function handleTastingNoteSubmit(){
-    event.preventDefault()
-    tastingNoteService.createTastingNote()
-    event.target.reset()
+// function handleTastingNoteSubmit(){
+//     event.preventDefault()
+//     tastingNoteService.createTastingNote()
+//     event.target.reset()
+// }
+
+// function handleSpiritSubmit(){
+//     event.preventDefault()
+//     spiritService.createSpirit()
+//     event.target.reset()
+// }
+const BASE_URL = "http://127.0.0.1:3000/spirits"
+const container = document.getElementById('spirits-list')
+
+function renderSpirits(spirit) {
+    const div = document.createElement('div')
+    div.id = `spirit-card-${spirit.id}`
+
+    const header = document.createElement('h3')
+    header.textContent = `Name: ${spirit.name}`
+
+    const p = document.createElement('p')
+    p.id = `spirit-info-${spirit.id}`
+    p.textContent = `
+        Spirit: ${spirit.spirit} -
+        ABV: ${spirit.abv} %-
+        Origin: ${spirit.origin}
+    `
+    const t = document.getElementById('tasting-notes-container')
+
+    div.append(header,p,t)
+    container.appendChild(div)
 }
 
-function handleSpiritSubmit(){
-    event.preventDefault()
-    spiritService.createSpirit()
-    event.target.reset()
+function getSpirits(){
+    fetch(BASE_URL)
+    .then(resp => resp.json())
+    .then(spirits => {
+        spirits.forEach(renderSpirits)
+    })
 }
+
+function init(){
+    getSpirits()
+}
+
+init()
